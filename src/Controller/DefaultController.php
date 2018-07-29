23f54a88218aed0a33b42b0d164521760a58d1d0
yemiwebby <yemiwebby@gmail.com>
2018-07-29 15:37:56 +0100

    updated the api

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,7 +12,7 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig',[]);
+        return $this->render('default/index.html.twig');
 
     }
 }
